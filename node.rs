0841059b59fc364c2cd845122143cf6fcfd39687
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-03-24 06:15:09 -0300

    Update node.rs

--- node.rs
+++ node.rs
@@ -1,6 +1,8 @@
+
 lncli addinvoice --amt <amount in satoshis> --memo=’my first amp’ --amp
 lncli payinvoice --pay_req <the amp invoice created by the receiver>
 lncli sendpayment --amt <amount in satoshis> --dest <public key of receiver> --amp
 lncli payinvoice --pay_req <the amp invoice created by the receiver> --pay_addr <the sha256 hash of a random number>
 lncli payinvoice --pay_req <the amp invoice created by the receiver> --amp-reuse
+lncli payinvoicetoken  --pay_req <the amp invoice created by the receiver> --amp-reuse
   
